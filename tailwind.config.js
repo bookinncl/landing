4cd63740c660092bff5d5768c79054b09d7e4e03
bookinncl <gonzalo@allpaka.com>
2025-10-24 11:49:28 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}"
   ],
-  theme: {
-    extend: {
-      colors: {
-        primary: {
-          DEFAULT: "#111827",
-          soft: "#f3f4f6"
-        }
-      }
-    },
-  },
-  plugins: [],
+  theme: { extend: {} },
+  plugins: []
 };
